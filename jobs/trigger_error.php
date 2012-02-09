8bb6a9cd0db1cd1e7ef56cfcb17a0378270029f5
Ross Duggan <ross@rossduggan.ie>
2012-02-09 17:17:47 +0000

    Update jobs/trigger_error.php

--- jobs/trigger_error.php
+++ jobs/trigger_error.php
@@ -5,7 +5,7 @@ $ch = curl_init();
 // set URL and other appropriate options
 curl_setopt($ch, CURLOPT_URL, "http://ec2-107-21-195-176.compute-1.amazonaws.com/");
 curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Pintsize: 99"));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Pintsize: 99", sprintf("X-Time: %s", date('r'))));
 
 // grab URL and pass it to the browser
 curl_exec($ch);
