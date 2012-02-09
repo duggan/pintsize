ff75582220bc8d035649b14dd7e2d1082dbf1c1d
Ross Duggan <ross@rossduggan.ie>
2012-02-09 17:14:36 +0000

    Update jobs/trigger_error.php

--- jobs/trigger_error.php
+++ jobs/trigger_error.php
@@ -1,3 +1,14 @@
 <?php
 
-trigger_error("Testing custom cron!");
+$ch = curl_init();
+
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_URL, "http://ec2-107-21-195-176.compute-1.amazonaws.com/");
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Pintsize: 99"));
+
+// grab URL and pass it to the browser
+curl_exec($ch);
+
+// close cURL resource, and free up system resources
+curl_close($ch);
